b5257b1a1773d8aa6f7c7bd0b4b312cac079347f
JasonFitz <jfitzsimmons@dot.nyc.gov>
2015-09-22 14:11:03 -0400

    updates, September 2015

---
+++ webapi/VZ_WEBAPI/VZ_API/Repository/ZipcodeRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using VZ_API.Models;
+
+namespace VZ_API.Repository
+{
+    public class ZipcodeRepository : IZipcodeRepository
+    {
+        string conn = WebConfigurationManager.ConnectionStrings["DevGIS"].ToString();
+
+        public IEnumerable<Models.zipcode> GetAll()
+        {
+            List<zipcode> zipcodes = new List<zipcode>();
+            string query = string.Format("SELECT [ZIP], [X], [Y] FROM [tbl_ZipCode]");
+
+            using (SqlConnection con =
+                    new SqlConnection(conn))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        zipcode zip = new zipcode();
+                        zip.zip = reader.GetInt32(0);
+                        zip.X = reader.GetDouble(1);
+                        zip.Y = reader.GetDouble(2);
+                        zipcodes.Add(zip);
+                    }
+                }
+            }
+            return zipcodes.ToArray();
+        }
+
+        public Models.zipcode Get(int zipcode)
+        {
+            List<zipcode> zipcodes = new List<zipcode>();
+            string query = string.Format("SELECT [ZIP], [X], [Y] FROM [tbl_ZipCode] where [ZIP] = "+zipcode );
+
+            zipcode zip = new zipcode();
+
+            using (SqlConnection con =
+                    new SqlConnection(conn))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {                        
+                        zip.zip = reader.GetInt32(0);
+                        zip.X = reader.GetDouble(1);
+                        zip.Y = reader.GetDouble(2);
+                        zipcodes.Add(zip);
+                    }
+                }
+            }
+            return zip;
+        }
+    }
+}
